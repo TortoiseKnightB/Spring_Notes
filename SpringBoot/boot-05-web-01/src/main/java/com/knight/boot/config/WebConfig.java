b82d3410eeeea56c9ab02a597242be019da17537
TortoiseKnight <yang.liu22@outlook.com>
2022-06-12 14:20:18 +0800

    feat: 定制化原理

--- SpringBoot/boot-05-web-01/src/main/java/com/knight/boot/config/WebConfig.java
+++ SpringBoot/boot-05-web-01/src/main/java/com/knight/boot/config/WebConfig.java
@@ -2,16 +2,17 @@ package com.knight.boot.config;
 
 import com.knight.boot.converter.KnightMessageConverter;
 import com.knight.boot.interceptor.LoginInterceptor;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.accept.HeaderContentNegotiationStrategy;
 import org.springframework.web.accept.ParameterContentNegotiationStrategy;
-import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 import org.springframework.web.util.UrlPathHelper;
 
 import java.util.Arrays;
@@ -19,6 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+//@EnableWebMvc
 @Configuration(proxyBeanMethods = false)
 public class WebConfig implements WebMvcConfigurer {
 
@@ -69,5 +71,27 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginInterceptor())
                 .addPathPatterns("/**")  // 所有请求都被拦截包括静态资源
                 .excludePathPatterns("/", "/login", "/haha/**","/nologin"); // 放行的请求
+    }
+
+
+    @Bean
+    public WebMvcRegistrations webMvcRegistrations(){
+        return new WebMvcRegistrations() {
+
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return WebMvcRegistrations.super.getRequestMappingHandlerMapping();
+            }
+
+            @Override
+            public RequestMappingHandlerAdapter getRequestMappingHandlerAdapter() {
+                return WebMvcRegistrations.super.getRequestMappingHandlerAdapter();
+            }
+
+            @Override
+            public ExceptionHandlerExceptionResolver getExceptionHandlerExceptionResolver() {
+                return WebMvcRegistrations.super.getExceptionHandlerExceptionResolver();
+            }
+        };
     }
 }
