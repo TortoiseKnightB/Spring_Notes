73711301eed276ed3ceaa4455633b58652779c7f
TortoiseKnight <yang.liu22@outlook.com>
2022-06-12 11:28:56 +0800

    feat: 嵌入式Servlet容器

--- SpringBoot/boot-05-web-01/src/main/java/com/knight/boot/Boot05Web01Application.java
+++ SpringBoot/boot-05-web-01/src/main/java/com/knight/boot/Boot05Web01Application.java
@@ -2,8 +2,9 @@ package com.knight.boot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
-
+@ServletComponentScan(basePackages = "com.knight.boot")
 @SpringBootApplication
 public class Boot05Web01Application {
 
