18a0f71cbd45776ade3fd37b09466d6b4d60a5eb
TortoiseKnight <yang.liu22@outlook.com>
2022-08-29 15:08:56 +0800

    feat: 添加一个小项目示例

--- SpringSecurity/README.md
+++ SpringSecurity/README.md
@@ -2,3 +2,10 @@
 
 - SpringSecurity 基础笔记
 - [官方文档](https://docs.spring.io/spring-security/site/docs/5.3.4.RELEASE/reference/html5/#servlet-hello)
+- [GitHub地址](https://github.com/spring-projects/spring-security)
+
+### 其他事项
+
+##### Spring Security 5.7.2 中已经废弃 WebSecurityConfigurerAdapter
+
+- 参考[官方建议方案](https://github.com/spring-projects/spring-security/issues/10822) 
---
+++ SpringSecurity/spring_security_5.7/src/main/java/com/knight/security/service/MyUserDetailService.java
@@ -0,0 +1,40 @@
+package com.knight.security.service;
+
+import com.knight.security.dao.UsersMapper;
+import com.knight.security.entity.Users;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author TortoiseKnightB
+ * @date 2022/08/15
+ */
+@Service("userDetailsService")
+public class MyUserDetailService implements UserDetailsService {
+
+    @Autowired
+    private UsersMapper usersMapper;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 根据用户名查询密码（数据库传功来的是加密的密码）。这里方便测试，手动对密码进行加密
+        Users users = usersMapper.queryUser(username);
+
+        if (users == null) {
+            throw new UsernameNotFoundException("用户名不存在！");
+        }
+
+        List<GrantedAuthority> authorities = AuthorityUtils.commaSeparatedStringToAuthorityList("admin");    // 设置用户权限(admins、manager、ROLE_sale、ROLE_admin)
+        User user = new User(users.getUsername(), new BCryptPasswordEncoder().encode(users.getPassword()), authorities);
+        return user;
+    }
+}
